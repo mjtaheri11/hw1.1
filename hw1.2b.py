3dc3defd3bc3713b7879056e1f5b2d48e9459709
mjtaheri11 <mj.taheri1996@gmail.com>
2020-03-04 17:12:28 +0330

    after task two partb

--- hw1.2b.py
+++ hw1.2b.py
@@ -1,55 +1,46 @@
+import numpy as np
 import cv2
+from skimage import data, filters
 
+cap = cv2.VideoCapture('video.mp4')
+frameIds = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25)
 
-cap = cv2.VideoCapture(0)
-# if (cap.isOpened() == False):
-#      print("Unable to read camera feed")
-
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-# while (True):
-#     ret, frame = cap.read()
-#     if ret == True:
-#         out.write(frame)
-#         cv2.imshow('frame', frame)
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-#         else:
-#             break
-# cap.release()
-# out.release()
-# Closes all the frames
-#cap = cv2.VideoCapture(0)
-
-#
-# while (True):
-#     ret, frame = cap.read()
-#     if ret == True:
-#         out.write(frame)
-#         cv2.imshow('frame', frame)
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-#         else:
-#             break
-# cap.release()
-# cv2.destroyAllWindows()
-while cap.isOpened():
+frames = []
+for fid in frameIds:
+    cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
     ret, frame = cap.read()
-    if ret == True:
-        cv2.imshow('frame', frame)
-        out.write(frame)
-        key = cv2.waitKey(27)
-        if key == ord('q'):
-            break
-        # elif key == ord('s'):
-        #     out.write(frame)
-    else:
-        break
+    frames.append(frame)
 
+medianFrame = np.median(frames, axis=0).astype(dtype=np.uint8)
+# cv2.imshow('frame', medianFrame)
+# cv2.waitKey(0)
 
+cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+grayMedianFrame = cv2.cvtColor(medianFrame, cv2.COLOR_BGR2GRAY)
+ret = True
+while ret:
+    ret, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    dframe = cv2.absdiff(frame, grayMedianFrame)
+    th, dframe = cv2.threshold(dframe, 30, 255, cv2.THRESH_BINARY)
+    cv2.imshow('frame', dframe)
+    cv2.waitKey(20)
 
 cap.release()
-#out.release()
 cv2.destroyAllWindows()
 
+
+#
+# cap = cv2.VideoCapture('video.mp4')
+#
+# while(cap.isOpened()):
+#     ret, frame = cap.read()
+#
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#     cv2.imshow('frame',gray)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
