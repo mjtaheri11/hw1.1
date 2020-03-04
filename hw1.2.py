979f9933fcecebcac80cea44fb647cac078a4a84
mjtaheri11 <mj.taheri1996@gmail.com>
2020-03-04 15:39:24 +0330

    after task 5 except line

--- hw1.2.py
+++ hw1.2.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def nothing(x):
@@ -6,6 +7,8 @@ def nothing(x):
 
 
 img = cv2.imread('space.jpg')
+img = cv2.resize(img, None, None, .5, .5, interpolation=cv2.INTER_LINEAR)
+# cv2.resize(img, (.5, .5))
 rows, cols, he = img.shape
 cv2.namedWindow('image')
 cv2.createTrackbar('test', 'image', 0, 360, nothing)
@@ -15,18 +18,24 @@ while True:
     k = cv2.waitKey(1) & 0xFF
     if k == ord('e'):
         break
-
+    color = (0, 255, 0)
+    thickness = 9
     x = cv2.getTrackbarPos('test', 'image')
+    start_point = (0, 0)
     M = cv2.getRotationMatrix2D((cols / 2, rows / 2), x, 1)
+    print(M)
+    end_point = (540, 304)
     rotated_img = cv2.warpAffine(img, M, (cols, rows))
-    cv2.imshow('image', rotated_img)
+    cv2.line(rotated_img, start_point, end_point, color, thickness)
+    both = np.hstack((img, rotated_img))
+    cv2.imshow('image', both)
+
+k = cv2.waitKey(0)
+if k == ord('e'):
+    cv2.destroyAllWindows()
     # k = cv2.waitKey(0)
     # if k == ord('e'):  # wait for 'e' key to exit
     #     cv2.destroyAllWindows()
-
-k = cv2.waitKey(0)
-if k == ord('e'):  # wait for 'e' key to exit
-    cv2.destroyAllWindows()
 
 # num_rows, num_cols = img.shape[:2]
 # cv2.namedWindow('frame')
---
+++ hw1.2b.py
@@ -0,0 +1,55 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+# if (cap.isOpened() == False):
+#      print("Unable to read camera feed")
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+# while (True):
+#     ret, frame = cap.read()
+#     if ret == True:
+#         out.write(frame)
+#         cv2.imshow('frame', frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#         else:
+#             break
+# cap.release()
+# out.release()
+# Closes all the frames
+#cap = cv2.VideoCapture(0)
+
+#
+# while (True):
+#     ret, frame = cap.read()
+#     if ret == True:
+#         out.write(frame)
+#         cv2.imshow('frame', frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#         else:
+#             break
+# cap.release()
+# cv2.destroyAllWindows()
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == True:
+        cv2.imshow('frame', frame)
+        out.write(frame)
+        key = cv2.waitKey(27)
+        if key == ord('q'):
+            break
+        # elif key == ord('s'):
+        #     out.write(frame)
+    else:
+        break
+
+
+
+cap.release()
+#out.release()
+cv2.destroyAllWindows()
+
