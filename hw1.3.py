39ba45855a3dbed2b40923cf2a40c989722be006
mjtaheri11 <mj.taheri1996@gmail.com>
2020-03-15 02:00:27 +0330

    it's done

--- hw1.3.py
+++ hw1.3.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 img = cv2.imread('2.jpg')
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 width = 300
 heigth = 400
 resized = cv2.resize(img, (heigth, width), interpolation = cv2.INTER_AREA)
@@ -18,15 +19,18 @@ output1 = cv2.filter2D(resized, -1, k1)
 #_,thresh2 = cv2.threshold(filter_image, 90, 255, cv2.THRESH_BINARY)
 output2 = cv2.filter2D(resized, -1, k2)
 
+output = resized.astype(np.float32) - output0.astype(np.float32)
+#output = resized = output0
+output = output.astype(np.uint8)
+
 cv2.imshow('output', output0)
 cv2.imwrite('output.jpg', output0)
 cv2.waitKey(0)
 cv2.imshow('output', output1)
-cv2.imwrite('output.jpg', output0)
+cv2.imwrite('output0.jpg', output1)
 cv2.waitKey(0)
 cv2.imshow('output', output2)
-cv2.imwrite('output.jpg', output0)
+cv2.imwrite('output.jpg', output2)
 cv2.waitKey(0)
-output = output0 + output1 + output2
 cv2.imshow('output', output)
 cv2.waitKey(0)
